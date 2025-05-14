1de8901351bfc752b079f03163a1007da9c3598f
Dmitriy S <d.sagachko@gmail.com>
2025-05-14 11:02:11 +0300

    Add SubscriptionRepository and its interface

---
+++ app/Repositories/SubscriptionRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Subscription;
+use Illuminate\Support\Collection;
+
+class SubscriptionRepository implements SubscriptionRepositoryInterface
+{
+    public function create(array $data): Subscription
+    {
+        return Subscription::query()->create($data);
+    }
+
+    public function update(Subscription $sub, array $data): bool
+    {
+        return $sub->update($data);
+    }
+
+    public function findByConfirmToken(string $token): ?Subscription
+    {
+        return Subscription::query()->where('confirm_token', $token)->first();
+    }
+
+    public function findByUnsubscribeToken(string $token): ?Subscription
+    {
+        return Subscription::query()->where('unsubscribe_token', $token)->first();
+    }
+
+    public function findAllConfirmed(): Collection
+    {
+        return Subscription::query()->where('confirmed', true)->get();
+    }
+}
---
+++ app/Repositories/SubscriptionRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Subscription;
+use Illuminate\Support\Collection;
+
+interface SubscriptionRepositoryInterface
+{
+    public function create(array $data): Subscription;
+    public function update(Subscription $sub, array $data): bool;
+    public function findByConfirmToken(string $token): ?Subscription;
+    public function findByUnsubscribeToken(string $token): ?Subscription;
+    public function findAllConfirmed(): Collection;
+}
