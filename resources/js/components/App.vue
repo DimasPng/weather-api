b1b0b45474645d6b09b8a812db5c62e0e6ab8dcf
Dmitriy S <d.sagachko@gmail.com>
2025-05-17 08:36:47 +0300

    Add Vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-container>
+                <v-text-field label="Ваше имя" />
+                <v-btn color="primary" class="mt-4">Отправить</v-btn>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,27 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+            port: 5173,
+        },
+    },
 });
