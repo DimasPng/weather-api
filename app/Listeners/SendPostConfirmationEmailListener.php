6631b704a79d1d6259b78b3f87d848c6eb377b42
Dmitriy S <d.sagachko@gmail.com>
2025-05-18 10:15:30 +0300

    Add subscription event listeners for email notifications

---
+++ app/Listeners/SendPostConfirmationEmailListener.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\SubscriptionConfirmedEvent;
+use App\Mail\ConfirmedSubscription;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Mail;
+
+class SendPostConfirmationEmailListener implements ShouldQueue
+{
+
+    public function __construct()
+    {
+    }
+
+    public function handle(SubscriptionConfirmedEvent $event): void
+    {
+        Mail::to($event->subscription->email)->send(new ConfirmedSubscription($event->subscription));
+    }
+}
---
+++ app/Listeners/SendWelcomeMail.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\SubscriptionCreatedEvent;
+use App\Mail\ConfirmSubscription;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Mail;
+
+class SendWelcomeMail implements ShouldQueue
+{
+    public function __construct()
+    {
+    }
+
+    public function handle(SubscriptionCreatedEvent $event): void
+    {
+        Mail::to($event->subscription->email)->send(new ConfirmSubscription($event->subscription));
+    }
+}
