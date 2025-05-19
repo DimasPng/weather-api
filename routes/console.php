7e0c6c67347210b65f611fd4c90080df568c90b8
Dmitriy S <d.sagachko@gmail.com>
2025-05-19 08:42:05 +0300

    Refactor schedule and remove UnsubscribeNotification class

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('hourly-update-command')->hourly();
-Schedule::command('daily-update-command')->daily();
+Schedule::command('daily-update-command')->daily()->dailyAt('9:00');
